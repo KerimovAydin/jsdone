f498b291e694339cae2e9d150337b1790d81552a
KerimovAydin <kerimovaydin177@gmail.com>
2026-04-12 02:02:53 +0000

    don

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
+
+    $('h2').click(function (e) {
+        e.preventDefault();
+        $('h2').html(1234567);
+    })
+
+    console.log($('input').val());
+
     $(document).on("keypress", function(e) {
         if (e.which == 13) {
-            let url = ``;
+            let url = `https://apidemo-wine.vercel.app/api/news`;
             async function mynews() {
                 let responce = await fetch(url);
                 let data = await responce.json();
